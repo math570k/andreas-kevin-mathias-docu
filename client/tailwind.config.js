5e409b40111a7e506513c39c22cd12e203149167
Kevin Mangkornkaew <kem@morningtrain.dk>
2020-11-19 13:59:29 +0100

    fixed tailwind and added postcss nesting and import

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,31 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
-  theme: {
-    colors: {
-      blue: {
-        default: "#252F3F",
-        darker: "#121B29"
-      },
-      white: "#fff",
-      gray: "#8C929D"
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                black: {
+                  default: "#121B29",
+                  100: "#71767f",
+                  200: "#595f69",
+                  300: "#414954",
+                  400: "#2a323e",
+                  500: "#121B29",
+                  600: "#101825",
+                  700: "#0e1621",
+                  800: "#0d131d",
+                  900: "#0b1019",
+                },
+                white: "#fff",
+                gray: "#8C929D"
+            },
+        },
     },
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+    corePlugins: {
+        preflight: false,
+    }
 }
